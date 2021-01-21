c4a41043395dbb9ed3ca51680c335b10b1ee8a25
Ragnar-Oock <u.drgnk@outlook.com>
2021-01-21 19:08:09 +0100

    remove unessessary error handling on songs.interpretations.get as connexion handle it for us

--- endpoints/songs/interpretations/endpoint.py
+++ endpoints/songs/interpretations/endpoint.py
@@ -9,13 +9,8 @@ def put(song, score):
 
 
 def get(song, padding, orderby, limit=50):
-    try:
-        interpretations = Interpretation\
-            .select(lambda i: i.song == Song[song])\
-            .order_by(format_order_by(orderby, 'i'))\
-            .limit(limit=limit, offset=padding)
-        return {'data': [i.serialize() for i in interpretations]}, 200
-    except AttributeError as e:
-        return {
-            'message': 'Error in orderby parameter, interpretation has no attribute \'{}\''.format(str(e).split('.')[1])
-        }
+    interpretations = Interpretation\
+        .select(lambda i: i.song == Song[song])\
+        .order_by(format_order_by(orderby, 'i'))\
+        .limit(limit=limit, offset=padding)
+    return {'data': [i.serialize() for i in interpretations]}, 200
