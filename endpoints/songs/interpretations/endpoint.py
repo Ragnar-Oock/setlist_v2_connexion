71b7905a4d3fa7a782de4f8215b623b18a032e5f
Ragnar-Oock <u.drgnk@outlook.com>
2021-01-21 18:05:10 +0100

    implement order by, limit and padding parameter for endpoint GET interpretation

--- endpoints/songs/interpretations/endpoint.py
+++ endpoints/songs/interpretations/endpoint.py
@@ -1,4 +1,5 @@
 from models import Interpretation, Song
+from utils.db import format_order_by
 
 
 def put(song, score):
@@ -7,6 +8,14 @@ def put(song, score):
     return '', 204
 
 
-def get(song):
-    interpretations = Interpretation.select(lambda i: i.song == Song[song])
-    return {'data': [i.serialize() for i in interpretations]}, 200
+def get(song, padding, orderby, limit=50):
+    try:
+        interpretations = Interpretation\
+            .select(lambda i: i.song == Song[song])\
+            .order_by(format_order_by(orderby, 'i'))\
+            .limit(limit=limit, offset=padding)
+        return {'data': [i.serialize() for i in interpretations]}, 200
+    except AttributeError as e:
+        return {
+            'message': 'Error in orderby parameter, interpretation has no attribute \'{}\''.format(str(e).split('.')[1])
+        }
