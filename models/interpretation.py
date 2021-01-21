86d949a4b1704aec1b366dbc5e290bd09687fa37
Ragnar-Oock <u.drgnk@outlook.com>
2021-01-21 18:13:14 +0100

    hide song id by default in serialized interpretation

--- models/interpretation.py
+++ models/interpretation.py
@@ -10,9 +10,16 @@ class Interpretation(db.Entity):
     date = orm.Required(datetime.datetime, sql_default='CURRENT_TIMESTAMP')
     score = orm.Required(Decimal, precision=4, scale=2)
 
-    def serialize(self):
-        return {
-            "song": self.song.id,
+    def serialize(self, show_song=False) -> dict:
+        """
+        serialize the current object as a dict
+        :param show_song: should the parent song id be displayed
+        :return: serialized Interpretation object
+        """
+        serialized = {
             "date": self.date,
             "score": self.score
         }
+        if show_song:
+            serialized["song"] = self.song.id
+        return serialized
