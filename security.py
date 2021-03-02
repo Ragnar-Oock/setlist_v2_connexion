2da08c11fd468778ed56c2c0debb41f783fc0dfc
Ragnar-Oock <u.drgnk@outlook.com>
2021-03-02 16:23:11 +0100

    update token validation to fully comply with the twitch Oauth client credential flow

--- security.py
+++ security.py
@@ -1,22 +1,39 @@
 import requests
+import logging
+import os
 
-from connexion.apps import flask_app
-
-
-#
-# from main import app
+logger = logging.getLogger(__name__)
 
 
 def verify_token(token):
+    # validate the token with the Twitch API
     response = requests.get(
         'https://id.twitch.tv/oauth2/validate',
         headers={'Authorization': 'OAuth {}'.format(token)}
     )
+
+    # the token is a valide token
     if response.status_code == requests.codes.ok:
-        flask_app.logger.debug('Oauth success')
+        body = response.json()
 
-        return {'client_id': response.json, 'scope': []}
+        # the token belong to this application
+        if body['client_id'] == os.environ['CLIENT_ID']:
+
+            # if the token is not a implicit code flow token (there's no user info in the response)
+            if body.get('user_id') is None and body.get('login') is None:
+                logger.info('Oauth success')
+
+                return {'client_id': response.json, 'scope': []}
+            # there's user info in the response, not a client credential flow token, reject the request
+            else:
+                return None
+
+        # the token doesn't belong to this application, reject the request
+        else:
+            logger.info('Oauth fail, token has been generated with an unknown client id : {}'.format(body['client_id']))
+            return None
+
+    # the validation request failed
     else:
-        flask_app.logger.debug('Oauth fail')
-
+        logger.info('Oauth fail, token is invalid')
         return None
